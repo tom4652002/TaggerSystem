49d8ccc8f31586bc40a242c5a61c88945097c005
torrsar <torrsar@USER>
2016-07-10 14:58:59 +0800

    加入專案檔案。

---
+++ CHTTaggerSystem/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace CHTTaggerSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.啟動ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.關於ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.結束XToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.結束XToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.結束XToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.復原UToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.取消復原RToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.剪下ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.複製ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.貼上PToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.刪除DToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.全選AToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.啟動ToolStripMenuItem,
+            this.關於ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 啟動ToolStripMenuItem
+            // 
+            this.啟動ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.結束XToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.結束XToolStripMenuItem1,
+            this.toolStripSeparator2,
+            this.結束XToolStripMenuItem2});
+            this.啟動ToolStripMenuItem.Name = "啟動ToolStripMenuItem";
+            this.啟動ToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.啟動ToolStripMenuItem.Text = "檔案(F)";
+            // 
+            // 關於ToolStripMenuItem
+            // 
+            this.關於ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.復原UToolStripMenuItem,
+            this.取消復原RToolStripMenuItem,
+            this.toolStripSeparator3,
+            this.剪下ToolStripMenuItem,
+            this.複製ToolStripMenuItem,
+            this.貼上PToolStripMenuItem,
+            this.刪除DToolStripMenuItem,
+            this.toolStripSeparator4,
+            this.全選AToolStripMenuItem});
+            this.關於ToolStripMenuItem.Name = "關於ToolStripMenuItem";
+            this.關於ToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.關於ToolStripMenuItem.Text = "編輯(E)";
+            // 
+            // 結束XToolStripMenuItem
+            // 
+            this.結束XToolStripMenuItem.Name = "結束XToolStripMenuItem";
+            this.結束XToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.結束XToolStripMenuItem.Text = "讀檔(L)";
+            // 
+            // 結束XToolStripMenuItem1
+            // 
+            this.結束XToolStripMenuItem1.Name = "結束XToolStripMenuItem1";
+            this.結束XToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.結束XToolStripMenuItem1.Text = "存檔(S)";
+            // 
+            // 結束XToolStripMenuItem2
+            // 
+            this.結束XToolStripMenuItem2.Name = "結束XToolStripMenuItem2";
+            this.結束XToolStripMenuItem2.Size = new System.Drawing.Size(152, 22);
+            this.結束XToolStripMenuItem2.Text = "結束(X)";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(149, 6);
+            // 
+            // 復原UToolStripMenuItem
+            // 
+            this.復原UToolStripMenuItem.Name = "復原UToolStripMenuItem";
+            this.復原UToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.復原UToolStripMenuItem.Text = "復原(U)";
+            // 
+            // 取消復原RToolStripMenuItem
+            // 
+            this.取消復原RToolStripMenuItem.Name = "取消復原RToolStripMenuItem";
+            this.取消復原RToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.取消復原RToolStripMenuItem.Text = "取消復原(R)";
+            // 
+            // 剪下ToolStripMenuItem
+            // 
+            this.剪下ToolStripMenuItem.Name = "剪下ToolStripMenuItem";
+            this.剪下ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.剪下ToolStripMenuItem.Text = "剪下(T)";
+            // 
+            // 複製ToolStripMenuItem
+            // 
+            this.複製ToolStripMenuItem.Name = "複製ToolStripMenuItem";
+            this.複製ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.複製ToolStripMenuItem.Text = "複製(C)";
+            // 
+            // 貼上PToolStripMenuItem
+            // 
+            this.貼上PToolStripMenuItem.Name = "貼上PToolStripMenuItem";
+            this.貼上PToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.貼上PToolStripMenuItem.Text = "貼上(P)";
+            // 
+            // 刪除DToolStripMenuItem
+            // 
+            this.刪除DToolStripMenuItem.Name = "刪除DToolStripMenuItem";
+            this.刪除DToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.刪除DToolStripMenuItem.Text = "刪除(D)";
+            // 
+            // 全選AToolStripMenuItem
+            // 
+            this.全選AToolStripMenuItem.Name = "全選AToolStripMenuItem";
+            this.全選AToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.全選AToolStripMenuItem.Text = "全選(A)";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(149, 6);
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(149, 6);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 411);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "TagSystem";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 啟動ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 關於ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 結束XToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem 結束XToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem 結束XToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem 復原UToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 取消復原RToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripMenuItem 剪下ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 複製ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 貼上PToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 刪除DToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripMenuItem 全選AToolStripMenuItem;
+    }
+}
+
